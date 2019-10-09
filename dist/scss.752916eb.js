9931293063211b7b70aa5686b4036da2ebf7814a
WenChiu <wen2214@gmail.com>
2019-10-09 17:26:15 +0800

    modify index page banner & slogan section

--- dist/scss.752916eb.js
+++ dist/scss.752916eb.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"/Users/wenchiu/tmot/mutix-service/assets/images/icon-facebook.png":[["icon-facebook.2f011cfc.png","assets/images/icon-facebook.png"],"assets/images/icon-facebook.png"],"/Users/wenchiu/tmot/mutix-service/assets/images/icon-services.png":[["icon-services.7b856460.png","assets/images/icon-services.png"],"assets/images/icon-services.png"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/Users/wenchiu/tmot/mutix-service/assets/images/icon-facebook.png":[["icon-facebook.2f011cfc.png","assets/images/icon-facebook.png"],"assets/images/icon-facebook.png"],"/Users/wenchiu/tmot/mutix-service/assets/images/banner-img.jpg":[["banner-img.166206b2.jpg","assets/images/banner-img.jpg"],"assets/images/banner-img.jpg"],"/Users/wenchiu/tmot/mutix-service/assets/images/icon-services.png":[["icon-services.7b856460.png","assets/images/icon-services.png"],"assets/images/icon-services.png"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54727" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49206" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
