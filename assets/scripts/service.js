82cbdbb0500065a139e430f2a1e23e3e9ec8d98c
jocelyn <rulinshyu@gmail.com>
2021-01-04 17:34:51 +0800

    feat(web): service page ui update

--- assets/scripts/service.js
+++ assets/scripts/service.js
@@ -4,9 +4,8 @@
   const buttons = slideContainer.querySelectorAll(".slide-button");
 
   function changeSlide(button) {
-    const activeClass = button.dataset.slide;
-    slideContainer.classList.remove("package", "translate", "live-stream", "transcript", "internet");
-    slideContainer.classList.add(activeClass);
+    const active = button.dataset.slide;
+    slideContainer.dataset.slide = active;
   }
 
   buttons.forEach(button => {
@@ -19,12 +18,19 @@
   const priceContainer = document.querySelector(".prices").querySelector(".container-main");
   const buttons = priceContainer.querySelectorAll(".price-button");
 
-  console.log(priceContainer);
+  function checkActive() {
+    const w = window.innerWidth;
+    if (w >= 1280) return 4;
+    if (w >= 1024) return 3;
+    if (w >= 768) return 2;
+    return 1
+  }
 
   function changePrice(button) {
     const active = parseInt(priceContainer.dataset.price, 10);
+    const maxActive = 5 - checkActive();
     const direction = parseInt(button.dataset.direction, 10);
-    priceContainer.dataset.price = active + direction;
+    priceContainer.dataset.price = Math.min(active, maxActive) + direction;
   }
 
   buttons.forEach(button => {
