f1039226cb328b0265a7d04bf03f0b468afb6926
jocelyn <rulinshyu@gmail.com>
2021-01-04 18:57:32 +0800

    feat(web): co-working space page

--- assets/scripts/floor-plan.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector(".price-floor-plan");
-
-container.addEventListener("click", ({ target }) => {
-  if (!target.classList.contains("floor-plan-button")) return;
-  const field = target.dataset.switch;
-  if (!field) return;
-  container.dataset[field] = container.dataset[field] === "1" ? "2" : "1";
-});
