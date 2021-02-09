f52bc05a83674d6b27f1c41a844a7a39536de49f
Can Yu <3246599-canyu@users.noreply.gitlab.com>
2021-02-10 02:08:49 +0800

    feat(web) slide autoplay

--- assets/scripts/slide.js
+++ assets/scripts/slide.js
@@ -14,3 +14,20 @@
     _button.addEventListener("click", () => changeActive(_button));
   });
 }
+
+// slide autoplay
+function nextSlide(container) {
+  const indexFromOne = 1;
+  const direction = 1;
+  const total = container.querySelectorAll(".slide").length;
+  var currentActive = parseInt(container.dataset.active, 10)
+  var nextActive = currentActive >= total ? indexFromOne : (currentActive + direction) % (total + indexFromOne);
+  container.dataset.active = nextActive;
+}
+
+setInterval(() => {
+  const containers = document.querySelectorAll(".slide-list");
+  containers.forEach(container => {
+    nextSlide(container);
+  })
+}, 3000);
